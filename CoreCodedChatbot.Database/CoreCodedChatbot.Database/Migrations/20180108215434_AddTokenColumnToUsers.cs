7cdbcf25a22e45e44b457ad2a35e1fb35bc91016
codedghost2 <codedghost2@gmail.com>
2019-10-19 13:51:05 +0100

    Initial Commit

---
+++ CoreCodedChatbot.Database/CoreCodedChatbot.Database/Migrations/20180108215434_AddTokenColumnToUsers.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoreCodedChatbot.Database.Migrations
+{
+    public partial class AddTokenColumnToUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TokenBytes",
+                table: "Users",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TokenBytes",
+                table: "Users");
+        }
+    }
+}
