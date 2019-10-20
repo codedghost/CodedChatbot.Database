87737b8fefa29daf513a526385534872063d9e90
codedghost2 <codedghost2@gmail.com>
2019-10-20 11:25:39 +0100

    Adding DI Package extension

---
+++ CoreCodedChatbot.Database/CoreCodedChatbot.Database/Package.cs
@@ -0,0 +1,16 @@
+﻿using CoreCodedChatbot.Database.Context;
+using CoreCodedChatbot.Database.Context.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CoreCodedChatbot.Database
+{
+    public static class Package
+    {
+        public static ServiceCollection AddDbContextFactory(this ServiceCollection services)
+        {
+            services.AddTransient<IChatbotContextFactory, ChatbotContextFactory>();
+
+            return services;
+        }
+    }
+}
