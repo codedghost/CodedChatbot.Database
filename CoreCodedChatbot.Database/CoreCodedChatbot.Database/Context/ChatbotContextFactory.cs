3444bf3f71fb8c866e982ac8f028553203fc7711
codedghost2 <codedghost2@gmail.com>
2019-11-10 11:17:31 +0000

    Updating factory to pass secret service

--- CoreCodedChatbot.Database/CoreCodedChatbot.Database/Context/ChatbotContextFactory.cs
+++ CoreCodedChatbot.Database/CoreCodedChatbot.Database/Context/ChatbotContextFactory.cs
@@ -1,12 +1,20 @@
 ﻿using CoreCodedChatbot.Database.Context.Interfaces;
+using CoreCodedChatbot.Secrets;
 
 namespace CoreCodedChatbot.Database.Context
 {
     public class ChatbotContextFactory : IChatbotContextFactory
     {
+        private readonly ISecretService _secretService;
+
+        public ChatbotContextFactory(ISecretService secretService)
+        {
+            _secretService = secretService;
+        }
+
         public IChatbotContext Create()
         {
-            return new ChatbotContext();
+            return new ChatbotContext(_secretService);
         }
     }
 }
