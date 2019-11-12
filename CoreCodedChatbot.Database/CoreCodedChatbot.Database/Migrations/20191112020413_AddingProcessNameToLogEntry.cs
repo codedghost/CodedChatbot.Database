1285f82243539e2d72b04fff838fe32f2a6c571e
codedghost2 <codedghost2@gmail.com>
2019-11-12 02:04:29 +0000

    Adding ProcessName to Log table

---
+++ CoreCodedChatbot.Database/CoreCodedChatbot.Database/Migrations/20191112020413_AddingProcessNameToLogEntry.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoreCodedChatbot.Database.Migrations
+{
+    public partial class AddingProcessNameToLogEntry : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProcessName",
+                table: "LogEntry",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProcessName",
+                table: "LogEntry");
+        }
+    }
+}
