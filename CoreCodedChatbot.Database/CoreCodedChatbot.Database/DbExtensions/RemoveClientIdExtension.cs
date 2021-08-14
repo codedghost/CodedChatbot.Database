6d39c8b72d24083bba30ffcfedd60af713163d9a
codedghost2 <codedghost2@gmail.com>
2021-08-14 16:27:16 +0100

    Adding clientId removal extension

---
+++ CoreCodedChatbot.Database/CoreCodedChatbot.Database/DbExtensions/RemoveClientIdExtension.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using CoreCodedChatbot.Database.Context.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreCodedChatbot.Database.DbExtensions
+{
+    public static class RemoveClientIdExtension
+    {
+        public static void RemoveClientId(this DbSet<User> users, string hubType, string clientId)
+        {
+            var user = users.SingleOrDefault(u => u._clientIds.Contains(clientId));
+
+            user?.ClientIds[hubType].Remove(clientId);
+        }
+    }
+}
